33a24b54d2cf24f77e9dc4727c7ce5802e5bfd62
Coulton.Fraser <coulton.fraser@ovintiv.com>
2023-12-01 12:52:22 -0700

    move code from github

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from "@astrojs/cloudflare";
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [react(), tailwind()],
 });
